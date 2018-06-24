8cda22c9f2eef04286e9116da5024ffbecce6162
JulianBastel <julian.skorka@googlemail.com>
2018-06-24 16:19:59 +0200

    update

--- plugins/system_controller/ir_ampswitch/index.js
+++ plugins/system_controller/ir_ampswitch/index.js
@@ -8,7 +8,8 @@ var execSync = require('child_process').execSync;
 
 
 module.exports = irAmpswitch;
-function irAmpswitch(context) {
+function irAmpswitch(context)
+{
 	var self = this;
 
 	this.context = context;
@@ -16,6 +17,8 @@ function irAmpswitch(context) {
 	this.logger = this.context.logger;
 	this.configManager = this.context.configManager;
 
+    self.logger.info("irAmpswitch");
+    
 }
 
 
@@ -25,15 +28,19 @@ irAmpswitch.prototype.onVolumioStart = function()
 	var configFile=this.commandRouter.pluginManager.getConfigurationFile(this.context,'config.json');
 	this.config = new (require('v-conf'))();
 	this.config.loadFile(configFile);
+    
+    self.logger.info("irAmpswitch.prototype.onVolumioStart");
 
     return libQ.resolve();
 }
 
 
-irAmpswitch.prototype.onStart = function() {
+irAmpswitch.prototype.onStart = function() 
+{
     var self = this;
 	var defer=libQ.defer();
 
+    self.logger.info("irAmpswitch.prototype.onStart");
 
 	// Once the Plugin has successfull started resolve the promise
 	defer.resolve();
@@ -41,9 +48,12 @@ irAmpswitch.prototype.onStart = function() {
     return defer.promise;
 };
 
-irAmpswitch.prototype.onStop = function() {
+irAmpswitch.prototype.onStop = function() 
+{
     var self = this;
     var defer=libQ.defer();
+    
+    self.logger.info("irAmpswitch.prototype.onStop");
 
     // Once the Plugin has successfull stopped resolve the promise
     defer.resolve();
@@ -51,22 +61,32 @@ irAmpswitch.prototype.onStop = function() {
     return libQ.resolve();
 };
 
-irAmpswitch.prototype.onRestart = function() {
+irAmpswitch.prototype.onRestart = function() 
+{
+    
     var self = this;
     // Optional, use if you need it
+    self.logger.info("irAmpswitch.prototype.onRestart");
 };
 
 
 // Configuration Methods -----------------------------------------------------------------------------
-irAmpswitch.prototype.saveIROptions = function (data) {
+irAmpswitch.prototype.saveOptions = function (data) 
+{
 	var self = this;
+    
+    self.logger.info("irAmpswitch.prototype.saveOptions");
+    
+    self.commandRouter.pushToastMessage('info', "saveOptions", "called saveOptions");
 }
 
 
-irAmpswitch.prototype.getUIConfig = function() {
+irAmpswitch.prototype.getUIConfig = function() 
+{
     var defer = libQ.defer();
     var self = this;
-
+    self.logger.info("irAmpswitch.prototype.getUIConfig");
+        
     var lang_code = this.commandRouter.sharedVars.get('language_code');
 
     self.commandRouter.i18nJson(__dirname+'/i18n/strings_'+lang_code+'.json',
@@ -86,177 +106,29 @@ irAmpswitch.prototype.getUIConfig = function() {
     return defer.promise;
 };
 
-irAmpswitch.prototype.getConfigurationFiles = function() {
+irAmpswitch.prototype.getConfigurationFiles = function() 
+{
+    self.logger.info("irAmpswitch.prototype.getConfigurationFiles");
 	return ['config.json'];
 }
 
-irAmpswitch.prototype.setUIConfig = function(data) {
+irAmpswitch.prototype.setUIConfig = function(data) 
+{
 	var self = this;
 	//Perform your installation tasks here
+    self.logger.info("irAmpswitch.prototype.setUIConfig");
 };
 
-irAmpswitch.prototype.getConf = function(varName) {
+irAmpswitch.prototype.getConf = function(varName) 
+{
 	var self = this;
 	//Perform your installation tasks here
+    self.logger.info("irAmpswitch.prototype.getConf");
 };
 
-irAmpswitch.prototype.setConf = function(varName, varValue) {
+irAmpswitch.prototype.setConf = function(varName, varValue) 
+{
 	var self = this;
 	//Perform your installation tasks here
-};
-
-
-
-// Playback Controls ---------------------------------------------------------------------------------------
-// If your plugin is not a music_sevice don't use this part and delete it
-
-
-irAmpswitch.prototype.addToBrowseSources = function () {
-
-	// Use this function to add your music service plugin to music sources
-    //var data = {name: 'Spotify', uri: 'spotify',plugin_type:'music_service',plugin_name:'spop'};
-    this.commandRouter.volumioAddToBrowseSources(data);
-};
-
-irAmpswitch.prototype.handleBrowseUri = function (curUri) {
-    var self = this;
-
-    //self.commandRouter.logger.info(curUri);
-    var response;
-
-
-    return response;
-};
-
-
-
-// Define a method to clear, add, and play an array of tracks
-irAmpswitch.prototype.clearAddPlayTrack = function(track) {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'irAmpswitch::clearAddPlayTrack');
-
-	self.commandRouter.logger.info(JSON.stringify(track));
-
-	return self.sendSpopCommand('uplay', [track.uri]);
-};
-
-irAmpswitch.prototype.seek = function (timepos) {
-    this.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'irAmpswitch::seek to ' + timepos);
-
-    return this.sendSpopCommand('seek '+timepos, []);
-};
-
-// Stop
-irAmpswitch.prototype.stop = function() {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'irAmpswitch::stop');
-
-
-};
-
-// Spop pause
-irAmpswitch.prototype.pause = function() {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'irAmpswitch::pause');
-
-
-};
-
-// Get state
-irAmpswitch.prototype.getState = function() {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'irAmpswitch::getState');
-
-
-};
-
-//Parse state
-irAmpswitch.prototype.parseState = function(sState) {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'irAmpswitch::parseState');
-
-	//Use this method to parse the state and eventually send it with the following function
-};
-
-// Announce updated State
-irAmpswitch.prototype.pushState = function(state) {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'irAmpswitch::pushState');
-
-	return self.commandRouter.servicePushState(state, self.servicename);
-};
-
-
-irAmpswitch.prototype.explodeUri = function(uri) {
-	var self = this;
-	var defer=libQ.defer();
-
-	// Mandatory: retrieve all info for a given URI
-
-	return defer.promise;
-};
-
-irAmpswitch.prototype.getAlbumArt = function (data, path) {
-
-	var artist, album;
-
-	if (data != undefined && data.path != undefined) {
-		path = data.path;
-	}
-
-	var web;
-
-	if (data != undefined && data.artist != undefined) {
-		artist = data.artist;
-		if (data.album != undefined)
-			album = data.album;
-		else album = data.artist;
-
-		web = '?web=' + nodetools.urlEncode(artist) + '/' + nodetools.urlEncode(album) + '/large'
-	}
-
-	var url = '/albumart';
-
-	if (web != undefined)
-		url = url + web;
-
-	if (web != undefined && path != undefined)
-		url = url + '&';
-	else if (path != undefined)
-		url = url + '?';
-
-	if (path != undefined)
-		url = url + 'path=' + nodetools.urlEncode(path);
-
-	return url;
-};
-
-
-
-
-
-irAmpswitch.prototype.search = function (query) {
-	var self=this;
-	var defer=libQ.defer();
-
-	// Mandatory, search. You can divide the search in sections using following functions
-
-	return defer.promise;
-};
-
-irAmpswitch.prototype._searchArtists = function (results) {
-
-};
-
-irAmpswitch.prototype._searchAlbums = function (results) {
-
-};
-
-irAmpswitch.prototype._searchPlaylists = function (results) {
-
-
-};
-
-irAmpswitch.prototype._searchTracks = function (results) {
-
+    self.logger.info("irAmpswitch.prototype.setConf");
 };
