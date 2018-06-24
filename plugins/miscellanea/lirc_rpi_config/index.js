e99c21fd82aea16bcf763b0ba3b90bfcbb301228
JulianBastel <julian.skorka@googlemail.com>
2018-06-24 19:52:56 +0200

    rtert

--- plugins/miscellanea/lirc_rpi_config/index.js
+++ plugins/miscellanea/lirc_rpi_config/index.js
@@ -4,17 +4,24 @@ var libQ = require('kew');
 var fs=require('fs-extra');
 var config = new (require('v-conf'))();
 var exec = require('child_process').exec;
-var execSync = require('child_process').execSync;
+var path = require('path');
+var os = require('os');
+var Gpio = require('onoff').Gpio;
+var sleep = require('sleep');
+var hwShutdown = false;
+var shutdownCtrl, initShutdown;
+var lircOverlayBanner = "#### RemotePi lirc setting below: do not alter ####" + os.EOL;
 
 
 module.exports = lircRpiConfig;
-function lircRpiConfig(context) {
-	var self = this;
+function lircRpiConfig(context) 
+{
+    var self = this;
 
-	this.context = context;
-	this.commandRouter = this.context.coreCommand;
-	this.logger = this.context.logger;
-	this.configManager = this.context.configManager;
+    this.context = context;
+    this.commandRouter = this.context.coreCommand;
+    this.logger = this.context.logger;
+    this.configManager = this.context.configManager;
 
 }
 
@@ -22,26 +29,28 @@ function lircRpiConfig(context) {
 
 lircRpiConfig.prototype.onVolumioStart = function()
 {
-	var self = this;
-	var configFile=this.commandRouter.pluginManager.getConfigurationFile(this.context,'config.json');
-	this.config = new (require('v-conf'))();
-	this.config.loadFile(configFile);
+    var self = this;
+    var configFile=this.commandRouter.pluginManager.getConfigurationFile(this.context,'config.json');
+    this.config = new (require('v-conf'))();
+    this.config.loadFile(configFile);
 
     return libQ.resolve();
 }
 
-lircRpiConfig.prototype.onStart = function() {
+lircRpiConfig.prototype.onStart = function() 
+{
     var self = this;
-	var defer=libQ.defer();
+    var defer=libQ.defer();
 
 
-	// Once the Plugin has successfull started resolve the promise
-	defer.resolve();
+    // Once the Plugin has successfull started resolve the promise
+    defer.resolve();
 
     return defer.promise;
 };
 
-lircRpiConfig.prototype.onStop = function() {
+lircRpiConfig.prototype.onStop = function() 
+{
     var self = this;
     var defer=libQ.defer();
 
@@ -51,29 +60,76 @@ lircRpiConfig.prototype.onStop = function() {
     return libQ.resolve();
 };
 
-lircRpiConfig.prototype.onRestart = function() {
+lircRpiConfig.prototype.onRestart = function() 
+{
     var self = this;
     // Optional, use if you need it
 };
 
 
 // Configuration Methods -----------------------------------------------------------------------------
+lircRpiConfig.prototype.saveConfig = function() 
+{
+    var self = this;
 
-lircRpiConfig.prototype.getUIConfig = function() {
+    actions.forEach
+    (
+        function(action, index, array) 
+        {
+             // Strings for data fields
+            var s1 = action.concat('Enabled');
+            var s2 = action.concat('Pin');
+
+            // Strings for config
+            var c1 = action.concat('.enabled');
+            var c2 = action.concat('.pin');
+            var c3 = action.concat('.value');
+
+            self.config.set(c1, data[s1]);
+            self.config.set(c2, data[s2]['value']);
+            self.config.set(c3, 0);
+        }
+    );
+
+    self.clearTriggers().then(self.createTriggers());
+    self.commandRouter.pushToastMessage('success',"lircRpiConfig", "Configuration saved");
+}
+
+
+
+
+lircRpiConfig.prototype.getUIConfig = function() 
+{
     var defer = libQ.defer();
     var self = this;
 
     var lang_code = this.commandRouter.sharedVars.get('language_code');
 
-    self.commandRouter.i18nJson(__dirname+'/i18n/strings_'+lang_code+'.json',
+    self.commandRouter.i18nJson(
+        __dirname+'/i18n/strings_'+lang_code+'.json',
         __dirname+'/i18n/strings_en.json',
-        __dirname + '/UIConfig.json')
-        .then(function(uiconf)
-        {
-
-
-            defer.resolve(uiconf);
-        })
+        __dirname + '/UIConfig.json'
+        )
+        .then(
+                function(uiconf)
+                {
+                    var i = 0;
+                    actions.forEach(function(action, index, array) 
+                    {
+                         // Strings for config
+                        var c1 = action.concat('.enabled');
+                        var c2 = action.concat('.pin');
+                        
+                        // accessor supposes actions and uiconfig items are in SAME order
+                        // this is potentially dangerous: rewrite with a JSON search of "id" value ?
+                        uiconf.sections[0].content[2*i].value         = self.config.get(c1);
+                        uiconf.sections[0].content[2*i+1].value.value = self.config.get(c2);
+                        uiconf.sections[0].content[2*i+1].value.label = self.config.get(c2).toString();
+                        i = i + 1;
+                    });
+                    defer.resolve(uiconf);
+                }
+        )
         .fail(function()
         {
             defer.reject(new Error());
@@ -82,176 +138,30 @@ lircRpiConfig.prototype.getUIConfig = function() {
     return defer.promise;
 };
 
-lircRpiConfig.prototype.getConfigurationFiles = function() {
-	return ['config.json'];
+lircRpiConfig.prototype.getConfigurationFiles = function() 
+{
+    return ['config.json'];
 }
 
-lircRpiConfig.prototype.setUIConfig = function(data) {
-	var self = this;
-	//Perform your installation tasks here
-};
-
-lircRpiConfig.prototype.getConf = function(varName) {
-	var self = this;
-	//Perform your installation tasks here
-};
-
-lircRpiConfig.prototype.setConf = function(varName, varValue) {
-	var self = this;
-	//Perform your installation tasks here
-};
-
-
-
-// Playback Controls ---------------------------------------------------------------------------------------
-// If your plugin is not a music_sevice don't use this part and delete it
-
-
-lircRpiConfig.prototype.addToBrowseSources = function () {
-
-	// Use this function to add your music service plugin to music sources
-    //var data = {name: 'Spotify', uri: 'spotify',plugin_type:'music_service',plugin_name:'spop'};
-    this.commandRouter.volumioAddToBrowseSources(data);
-};
-
-lircRpiConfig.prototype.handleBrowseUri = function (curUri) {
+lircRpiConfig.prototype.setUIConfig = function(data) 
+{
     var self = this;
+    //Perform your installation tasks here
+};
 
-    //self.commandRouter.logger.info(curUri);
-    var response;
+lircRpiConfig.prototype.getConf = function(varName) 
+{
+    var self = this;
+    //Perform your installation tasks here
+};
 
-
-    return response;
+lircRpiConfig.prototype.setConf = function(varName, varValue) 
+{
+    var self = this;
+    //Perform your installation tasks here
 };
 
 
-
-// Define a method to clear, add, and play an array of tracks
-lircRpiConfig.prototype.clearAddPlayTrack = function(track) {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'lircRpiConfig::clearAddPlayTrack');
-
-	self.commandRouter.logger.info(JSON.stringify(track));
-
-	return self.sendSpopCommand('uplay', [track.uri]);
-};
-
-lircRpiConfig.prototype.seek = function (timepos) {
-    this.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'lircRpiConfig::seek to ' + timepos);
-
-    return this.sendSpopCommand('seek '+timepos, []);
-};
-
-// Stop
-lircRpiConfig.prototype.stop = function() {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'lircRpiConfig::stop');
-
-
-};
-
-// Spop pause
-lircRpiConfig.prototype.pause = function() {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'lircRpiConfig::pause');
-
-
-};
-
-// Get state
-lircRpiConfig.prototype.getState = function() {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'lircRpiConfig::getState');
-
-
-};
-
-//Parse state
-lircRpiConfig.prototype.parseState = function(sState) {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'lircRpiConfig::parseState');
-
-	//Use this method to parse the state and eventually send it with the following function
-};
-
-// Announce updated State
-lircRpiConfig.prototype.pushState = function(state) {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'lircRpiConfig::pushState');
-
-	return self.commandRouter.servicePushState(state, self.servicename);
-};
-
-
-lircRpiConfig.prototype.explodeUri = function(uri) {
-	var self = this;
-	var defer=libQ.defer();
-
-	// Mandatory: retrieve all info for a given URI
-
-	return defer.promise;
-};
-
-lircRpiConfig.prototype.getAlbumArt = function (data, path) {
-
-	var artist, album;
-
-	if (data != undefined && data.path != undefined) {
-		path = data.path;
-	}
-
-	var web;
-
-	if (data != undefined && data.artist != undefined) {
-		artist = data.artist;
-		if (data.album != undefined)
-			album = data.album;
-		else album = data.artist;
-
-		web = '?web=' + nodetools.urlEncode(artist) + '/' + nodetools.urlEncode(album) + '/large'
-	}
-
-	var url = '/albumart';
-
-	if (web != undefined)
-		url = url + web;
-
-	if (web != undefined && path != undefined)
-		url = url + '&';
-	else if (path != undefined)
-		url = url + '?';
-
-	if (path != undefined)
-		url = url + 'path=' + nodetools.urlEncode(path);
-
-	return url;
-};
-
-
-
-
-
-lircRpiConfig.prototype.search = function (query) {
-	var self=this;
-	var defer=libQ.defer();
-
-	// Mandatory, search. You can divide the search in sections using following functions
-
-	return defer.promise;
-};
-
-lircRpiConfig.prototype._searchArtists = function (results) {
-
-};
-
-lircRpiConfig.prototype._searchAlbums = function (results) {
-
-};
-
-lircRpiConfig.prototype._searchPlaylists = function (results) {
-
-
-};
 
 lircRpiConfig.prototype._searchTracks = function (results) {
 
